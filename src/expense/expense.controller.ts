f608da6bed23cb0d619503398b63d5ab497ba173
Baver <bavertorunx@gmail.com>
2024-08-07 16:02:14 +0300

    feat: Added /expenses/update endpoint
    This enpoint allows us to update the spend.

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -1,20 +1,24 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { ExpenseService } from './expense.service';
 import { ExpenseDTO } from './dto/Expense.dto';
 
 @Controller('expenses')
 export class ExpenseController {
-    constructor(private readonly expenseService: ExpenseService){}
-    @Get('')
-    async getAllExpenses(){
-        return this.expenseService.getAllExpenses();
-    }
-    @Get(':id')
-    async getOneExpense(@Param('id') expenseId: string){
-        this.expenseService.getOneExpense(expenseId);
-    }
-    @Post('create')
-    async createExpense(@Body() body:ExpenseDTO){
-        this.expenseService.createExpense(body);
-    }
+  constructor(private readonly expenseService: ExpenseService) {}
+  @Get('')
+  async getAllExpenses() {
+    return this.expenseService.getAllExpenses();
+  }
+  @Get(':id')
+  async getOneExpense(@Param('id') expenseId: string) {
+    return this.expenseService.getOneExpense(expenseId);
+  }
+  @Post('create')
+  async createExpense(@Body() body: ExpenseDTO) {
+    return this.expenseService.createExpense(body);
+  }
+  @Put('update/:id')
+  async updateExpense(@Param('id') id: string, @Body() body: ExpenseDTO) {
+    return this.expenseService.updateExpense(id, body);
+  }
 }
--- src/expense/expense.service.ts
+++ src/expense/expense.service.ts
@@ -6,18 +6,25 @@ import { ExpenseDTO } from './dto/Expense.dto';
 
 @Injectable()
 export class ExpenseService {
-    constructor(@InjectModel(Expense.name) private readonly expenseModel: Model<Expense>){}
-    async getAllExpenses(){
-        const expenses = await this.expenseModel.find().exec();
-        return expenses;
-    }
-    async getOneExpense(expenseId:string){
-        const expense = await this.expenseModel.findById(expenseId);
-        return expense;
-    }
-    async createExpense(body: ExpenseDTO){
-        const newExpense = await this.expenseModel.create(body);
-        return newExpense;
-    }
-
+  constructor(
+    @InjectModel(Expense.name) private readonly expenseModel: Model<Expense>,
+  ) {}
+  async getAllExpenses() {
+    const expenses = await this.expenseModel.find().exec();
+    return expenses;
+  }
+  async getOneExpense(expenseId: string) {
+    const expense = await this.expenseModel.findById(expenseId);
+    return expense;
+  }
+  async createExpense(body: ExpenseDTO) {
+    const newExpense = await this.expenseModel.create(body);
+    return newExpense;
+  }
+  async updateExpense(id: string, body: ExpenseDTO) {
+    const updateExpense = this.expenseModel.findByIdAndUpdate(id, body, {
+      new: true,
+    });
+    return updateExpense;
+  }
 }
