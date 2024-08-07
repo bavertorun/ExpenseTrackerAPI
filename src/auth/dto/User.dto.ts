c6e02c93cfd1ebffa78d304627a4746cb36127c7
Baver <bavertorunx@gmail.com>
2024-08-07 12:52:02 +0300

    feat: Added User DTO

---
+++ src/auth/dto/User.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class UserDTO {
+  @IsNotEmpty()
+  username: string;
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+  @IsNotEmpty()
+  password: string;
+}
