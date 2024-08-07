c25c6c8281c5d7034b29ec67fddd7ea342476fb3
Baver <bavertorunx@gmail.com>
2024-08-07 16:13:09 +0300

    feat: Added JWT authentication guard to expense endpoints
    - Applied `@UseGuards(AuthGuard('jwt'))` to all expense-related endpoints.
    - Ensured that only authenticated users can access the expense routes.
    - Enhanced security by requiring JWT token validation for requests.

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -1,26 +1,32 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { ExpenseService } from './expense.service';
 import { ExpenseDTO } from './dto/Expense.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('expenses')
 export class ExpenseController {
   constructor(private readonly expenseService: ExpenseService) {}
+  @UseGuards(AuthGuard('jwt'))
   @Get('')
   async getAllExpenses() {
     return this.expenseService.getAllExpenses();
   }
+  @UseGuards(AuthGuard('jwt'))
   @Get(':id')
   async getOneExpense(@Param('id') expenseId: string) {
     return this.expenseService.getOneExpense(expenseId);
   }
+  @UseGuards(AuthGuard('jwt'))
   @Post('create')
   async createExpense(@Body() body: ExpenseDTO) {
     return this.expenseService.createExpense(body);
   }
+  @UseGuards(AuthGuard('jwt'))
   @Put('update/:id')
   async updateExpense(@Param('id') id: string, @Body() body: ExpenseDTO) {
     return this.expenseService.updateExpense(id, body);
   }
+  @UseGuards(AuthGuard('jwt'))
   @Delete('delete/:id')
   async deleteExpense(@Param('id') id: string){
     return this.expenseService.deleteExpense(id);
